6f463a0f835e055ead0cbbd0ceb37208eba2cfa6
Jaime-Sánchez <jaimeqsanchezc@gmail.com>
2021-06-16 19:24:16 -0400

    Generar interfaz de ver consolas y agregar consolas

--- web/zgames/resources/views/layouts/master.blade.php
+++ web/zgames/resources/views/layouts/master.blade.php
@@ -4,15 +4,19 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{csrf_token()}}">
 <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-<title>Z Games</title>
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <title>Z Games</title>
   </head>
   <body>
     <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light bg-warning">
+        <nav class="navbar navbar-expand-lg navbar-light bg-warning">
             <div class="container-fluid">
-              <a class="navbar-brand" href="{{route('home')}}">Z Games</a>
+              <a class="navbar-brand" href="{{route('home')}}">
+                <img class="logo" src="{{asset('img/logo.png')}}">
+            </a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
@@ -36,6 +40,8 @@
 
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="{{asset('js/axios_config.js')}}"></script>
 
   </body>
 </html>
